376dd18960398d9b9d1270968787881d98b57c84
Umang2023 <umangg.2023@gmail.com>
2021-10-08 23:54:45 +0530

    editor completed

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,3 +1,24 @@
+var userTheme = document.getElementById('themes');
+
+userTheme.addEventListener('change', () => {
+    var themeChoosen = userTheme.value;
+
+    if (themeChoosen === 'solarized_light') {
+        loadEditor('solarized_light')
+    }
+    else if (themeChoosen === 'clouds') {
+        loadEditor('clouds')
+    }
+    else if (themeChoosen === 'solarized_dark') {
+        loadEditor('solarized_dark')
+    }
+    else if (themeChoosen === 'tomorrow_night_blue') {
+        loadEditor('tomorrow_night_blue')
+    } else {
+        loadEditor('monokai')
+    }
+
+})
 function fillDashboard(data) {
     let profImg = document.createElement('img');
     profImg.src = data.pic;
@@ -11,7 +32,30 @@ function getDashboardData() {
         return res.json();
     }).then((data) => {
         fillDashboard(data);
+        loadEditor('monokai');
     })
+}
+function loadEditor(theme) {
+    var editor = ace.edit("editor");
+    editor.setTheme(`ace/theme/${theme}`);
+
+    setTemplate();
+}
+
+function setTemplate() {
+    var editor = ace.edit("editor");
+    var EditSession = require("ace/edit_session").EditSession;
+    var cpp = new EditSession("#include < iostream >\r\n" +
+        "using namespace std;\r\n" +
+        "\r\n" +
+        "int main() {\r\n" +
+        "\t// your code goes here\r\n" +
+        "\treturn 0;\r\n" +
+        "}\r\n");
+    // and then to load document into editor, just call
+    editor.setSession(cpp);
+    editor.session.setMode("ace/mode/c_cpp");
 }
 
 window.onload = getDashboardData();
+

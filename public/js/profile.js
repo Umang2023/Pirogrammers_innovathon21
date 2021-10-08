6d2fee4b63f1454cab900b237940eb4a1c18e993
Umang2023 <umangg.2023@gmail.com>
2021-10-08 15:57:08 +0530

    profile page updated

--- public/js/profile.js
+++ public/js/profile.js
@@ -14,24 +14,65 @@ function fillProfile(data) {
     document.querySelector('.name-info').innerHTML = profName;
     document.querySelector('.email-info').innerHTML = profEmail;
 
-    var codeforcesContent='',codechefContent='',leetcodeContent='',spojContent='',atcoderContent=''
+    var codeforcesContent = '', codechefContent = '', leetcodeContent = '', spojContent = '', atcoderContent = ''
 
-    if(data.codeforces && data.codeforces.length > 0)
-    codeforcesContent = `Codeforces: <a href="https://codeforces.com/profile/${data.codeforces}" target="_blank">${data.codeforces}</a>`;
+    if (data.codeforces && data.codeforces.length > 0)
+        codeforcesContent = `
+        <div class="handle_name"> Codeforces </div> 
+        <div class="handle_value">
+            :<a href="https://codeforces.com/profile/${data.codeforces}" target="_blank"> ${data.codeforces}
+        </a>
+        </div>`;
+    else {
+        codeforcesContent = `
+        <div class="handle_name"> Codeforces </div> 
+        <div class="handle_value">: Not set </div>`;
+    }
 
-
-    if(data.codechef && data.codechef.length > 0)
-    codechefContent = `Codechef: <a href="https://codechef.com/users/${data.codechef}" target="_blank">${data.codechef}</a>`;
-    
-    if(data.leetcode && data.leetcode.length > 0)
-    leetcodeContent = `Leetcode: <a href="https://leetcode.com/${data.leetcode}" target="_blank">${data.leetcode}</a>`;
-    
-    if(data.spoj && data.spoj.length > 0)
-    spojContent = `Spoj: <a href="https://spoj.com" target="_blank">${data.spoj}</a>`;
-    
-    if(data.atcoder && data.atcoder.length > 0)
-    atcoderContent = `Atcoder: <a href="https://atcoder.jp/users/${data.atcoder}" target="_blank">${data.atcoder}</a>`;
-
+    if (data.codechef && data.codechef.length > 0)
+        codechefContent = `
+        <div class="handle_name"> CodeChef </div>
+        <div class="handle_value"> 
+            :<a href="https://codechef.com/users/${data.codechef}" target="_blank"> ${data.codechef}</a>
+        </div>`;
+    else {
+        codechefContent = `
+        <div class="handle_name"> CodeChef </div> 
+        <div class="handle_value">: Not set </div>`;
+    }
+    if (data.leetcode && data.leetcode.length > 0)
+        leetcodeContent = `
+        <div class="handle_name"> Leetcode </div> 
+        <div class="handle_value">
+            :<a href="https://leetcode.com/${data.leetcode}" target="_blank"> ${data.leetcode}</a>
+        </div>`;
+    else {
+        leetcodeContent = `
+        <div class="handle_name"> Leetcode </div>
+        <div class="handle_value">: Not set </div>`;
+    }
+    if (data.spoj && data.spoj.length > 0)
+        spojContent = `
+        <div class="handle_name"> Spoj </div> 
+        <div class="handle_value">
+            :<a href="https://spoj.com" target="_blank"> ${data.spoj}</a>
+        </div>`;
+    else {
+        spojContent = `
+        <div class="handle_name"> Spoj </div> 
+        <div class="handle_value">: Not set</div>`;
+    }
+    if (data.atcoder && data.atcoder.length > 0)
+        atcoderContent = `
+        <div class="handle_name"> Atcoder </div> 
+        <div class="handle_value">
+            :<a href="https://atcoder.jp/users/${data.atcoder}" target="_blank"> ${data.atcoder}</a>
+        </div>`;
+    else {
+        atcoderContent = `
+        <div class="handle_name"> Atcoder </div> 
+        <div class="handle_value">: Not set</div>`;
+    }
     document.querySelector('.codeforces-handle').innerHTML = codeforcesContent;
     document.querySelector('.codechef-handle').innerHTML = codechefContent;
     document.querySelector('.leetcode-handle').innerHTML = leetcodeContent;
@@ -47,4 +88,7 @@ function getProfileData() {
     })
 }
 
+function editHandles() {
+    window.location.href = './setup'
+}
 window.onload = getProfileData();
